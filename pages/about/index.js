c4c1d0c706161a824951e32d56442f3418327bd7
Pratham <prathamjain340@gmail.com>
2024-09-21 02:22:07 +0530

    fixes

--- pages/about/index.js
+++ pages/about/index.js
@@ -230,15 +230,19 @@ const About = () => {
           className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
         >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-            {aboutData.map((item, index) => {
+            {aboutData.map((item, itemindex) => {
+              // {aboutData.map((item, index) => {
               return (
                 <div
-                  key={index}
+                  key={itemindex}
+                  // key={index}
                   className={`${
-                    index === item &&
+                    index === itemindex &&
+                    // index === item &&
                     "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
                   } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                  onClick={() => setIndex(itemIndex)}
+                  onClick={() => setIndex(itemindex)}
+                  // onClick={() => setIndex(itemIndex)}
                 >
                   {item.title}
                 </div>
